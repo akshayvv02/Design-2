0586ce5d64b66626afdd8cf3fea01a314fba7304
Akshay Vijayvergiya <akvv2596@gmail.com>
2025-05-16 17:59:34 -0400

    Problem-1-Queue.java updated with coments

--- Problem-1-Queue.java
+++ Problem-1-Queue.java
@@ -1,53 +1,47 @@
-class MyQueue {
+/*
+ * Implements a queue using two stacks (FIFO using LIFO).
+ * 1. input: holds newly pushed elements.
+ * 2. output: holds reversed elements for dequeueing.
 
-    Node head;
-    Node tail;
-    int size = 0;
-    public MyQueue() {
-        head = null;
-        tail = null;
-    }
-    
-    public void push(int x) {
-        Node node = new Node(x);
-        if(head == null)
-            head = node;
-        else
-            tail.next = node;
-        tail = node;
-        size++;
-    }
-    
-    public int pop() {
-        int temp = head.data;
-        head = head.next;
-        size--;
-        return temp;
-    }
-    
-    public int peek() {
-        return (head.data);
-    }
-    
-    public boolean empty() {
-        return (size == 0);
-    }
-}
-
-class Node{
-    int data;
-    Node next;
-
-    public Node(int data){
-        this.data = data;
-        this.next = null;
-    }
-}
-/**
- * Your MyQueue object will be instantiated and called as such:
- * MyQueue obj = new MyQueue();
- * obj.push(x);
- * int param_2 = obj.pop();
- * int param_3 = obj.peek();
- * boolean param_4 = obj.empty();
+ * - push(x): always push to input stack.
+ * - pop(): if output is empty, transfer all input to output (reversing order), then pop from output.     
+ * - peek(): same as pop, but returns top of output without removing it.
+ * - empty(): returns true if both stacks are empty.
  */
+
+// Time Complexity : O(1) - Amortized Time, O(n) - Worst Case Time	
+// Space Complexity : O(n)
+// Did this code successfully run on Leetcode : Yes
+// Any problem you faced while coding this : No
+
+class MyQueue {
+    private Stack<Integer> input;
+    private Stack<Integer> output;
+
+    public MyQueue() {
+        input = new Stack<>();
+        output = new Stack<>();
+    }
+
+    public void push(int x) {
+        input.push(x);
+    }
+
+    public int pop() {
+        peek();
+        return output.pop();
+    }
+
+    public int peek() {
+        if (output.isEmpty()) {
+            while (!input.isEmpty()) {
+                output.push(input.pop());
+            }
+        }
+        return output.peek();
+    }
+
+    public boolean empty() {
+        return input.isEmpty() && output.isEmpty();
+    }
+}
